b05e3afa323624fb00d406f27e6fb0995ac72446
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-18 13:08:10 +0600

    Product adding process done

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+var admin = require("firebase-admin");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 7007;
+
+// Firebase Admin Initialization 
+var serviceAccount = require('./carz-zone-69e6b-firebase-adminsdk-b4cob-02175f4c5a.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 // Middlewares
 app.use(cors());
@@ -13,6 +21,24 @@ app.use(express.json());
 // Connection URI 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwr1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Token Verification 
+const verifyToken = async (req, res, next) => {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const idToken = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(idToken);
+            console.log(decodedUser);
+            req.decodedUserEmail = decodedUser.email;
+        }
+        catch {
+            console.log('You are catched');
+        }
+    }
+    next();
+}
+
 
 async function run() {
     try {
@@ -25,26 +51,12 @@ async function run() {
         const productsCollection = database.collection('products');
         const usersCollection = database.collection('users');
 
-        // Read Operations 
+        // Read All Products 
         app.get('/products', async (req, res) => {
             console.log('Hitting products...')
             const cursor = await productsCollection.find({}).toArray();
             // console.log(cursor);
             res.send(cursor);
-        });
-
-        // Add a User 
-        app.post('/users', async (req, res) => {
-            console.log('Hitting the post', req.body);
-            const query = { email: `${req.body.email}` };
-            const result = await usersCollection.findOne(query);
-
-            if (result === null) {
-                const newUser = req.body;
-                const insertResult = await usersCollection.insertOne(newUser);
-                console.log(insertResult);
-                res.json(insertResult);
-            }
         });
 
         // Admin Checking 
@@ -62,6 +74,40 @@ async function run() {
                 res.json({ isAdmin: false });
             }
         })
+
+        // Add a User 
+        app.post('/users', async (req, res) => {
+            console.log('Hitting the post', req.body);
+            const query = { email: `${req.body.email}` };
+            const result = await usersCollection.findOne(query);
+
+            if (result === null) {
+                const newUser = req.body;
+                const insertResult = await usersCollection.insertOne(newUser);
+                console.log(insertResult);
+                res.json(insertResult);
+            }
+        });
+
+        // Add a Product 
+        app.post('/addProduct', verifyToken, async (req, res) => {
+            const email = req.query?.email;
+            const query = { email: `${email}` };
+            const result = await usersCollection.findOne(query);
+            console.log(result);
+
+            if ((req.decodedUserEmail === email) && (result?.role === 'admin')) {
+                // console.log('Hitting the post', req.body);
+                const newProduct = req.body;
+                const insertResult = await productsCollection.insertOne(newProduct);
+                console.log(insertResult);
+                res.json(insertResult);
+
+            }
+            else {
+                req.status(401).json({ message: 'User not authorized' });
+            }
+        });
 
 
     }
